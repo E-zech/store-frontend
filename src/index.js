a4c6ec07ca35a2432db22e3153ef5b41da6cbb61
E-zech <nemo557@gmail.com>
2024-05-17 13:21:00 +0300

    added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
